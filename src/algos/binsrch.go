8e924de3332335bc45e7c5d4a3c16582fc935101
Терещенко Павел <P.Tereschenko@carmoney.ru>
2023-07-17 13:10:51 +0300

    Ввод числа через консоль.

--- src/algos/binsrch.go
+++ src/algos/binsrch.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
-	var searchNum int64 = 8
-	lst := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 30, 40, 50, 1000000}
+	var searchNum int64
+	fmt.Print("Введи целое число (поиск среди массива от 0 до 20):\n")
+	fmt.Scanf("%d\n", &searchNum)
+
+	lst := []int64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 	resulIndex := searchNumberPosition(searchNum, lst)
 	if resulIndex == -1 {
 		fmt.Printf("Число %d не найдено", searchNum)
@@ -19,14 +22,12 @@ func main() {
 func searchNumberPosition(num int64, lst []int64) int64 {
 	var minIdx int64 = 0
 	var maxidx int64 = int64(len(lst) - 1)
-	iteration := 0
 	for minIdx <= maxidx {
-		fmt.Printf("%d \n", iteration)
 		midIdx := int64(math.Floor(float64(minIdx+maxidx) / 2))
 		midVal := lst[midIdx]
 
 		if num == midVal {
-			return maxidx
+			return midIdx
 		}
 
 		if num < midVal {
